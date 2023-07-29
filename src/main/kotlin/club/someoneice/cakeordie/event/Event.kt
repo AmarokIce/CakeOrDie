ddc0819fa6ea5ad36b63278a444829dd7b60d0e3
Amarok <91077200+AmarokIce@users.noreply.github.com>
2023-07-29 15:51:40 +0800

    Lang.

---
+++ src/main/kotlin/club/someoneice/cakeordie/event/Event.kt
@@ -0,0 +1,9 @@
+package club.someoneice.cakeordie.event
+
+import net.minecraftforge.event.entity.player.PlayerEvent
+
+class Event {
+    fun recipeEvent(event: PlayerEvent.ItemCraftedEvent) {
+
+    }
+}
